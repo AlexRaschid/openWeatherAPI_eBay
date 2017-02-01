b7ed3970fa810697c548826352884efb31e1bc87
Alexander Raschid <alexraschid1337@gmail.com>
2017-02-01 23:16:17 +0000

    Got the keys to print

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
 
 
-    $("#buttonInput").on( "click" ,function() {
+    $("#buttonInput").on("click", function(event) {
         var userInput = $("#userInput").val();
-
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=203c9b901d5404f094111c9da3767754";
         console.log(userInput);
-        $.getJSON(
-            "http://api.openweathermap.org/data/2.5/weather?q="
-            + userInput + "&appid=203c9b901d5404f094111c9da3767754",
-            function(results) {
-                console.log(userInput);
-                console.log(results);
+        console.log(url);
+        $.get(url, function(results) {
+            console.log(userInput);
+            console.log(results);
 
-                $("#weather-results").append("<h4>------------------------------</h4>");
-                $("#weather-results").append("<h4>" + results["main"]["temp"] + "</h4>");
-                $("#weather-results").append("<h4>" + results["main"]["pressure"] + "</h4>");
-                $("#weather-results").append("<h4>" + results["wind"]["speed"] + "</h4>");
+            $("#weather-results").append("<h4>------------------------------</h4>");
+            $("#weather-results").append("<h4>" + results["main"]["temp"] + "</h4>");
+            $("#weather-results").append("<h4>" + results["main"]["pressure"] + "</h4>");
+            $("#weather-results").append("<h4>" + results["wind"]["speed"] + "</h4>");
 
 
-            });
+        });
 
 
 
